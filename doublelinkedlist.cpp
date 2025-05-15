12362bf8f1794fbf8e558a720f9b9a5f1fbd6c3f
KaijuuYahoo <muhammad.rasyid.ft24@mail.umy.ac.id>
2025-05-15 15:17:02 +0700

    menambahkan alamat baru pada node dengan new node

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -11,5 +11,19 @@ class node {
 
 class DoubleLinkedList{
     private:
-        node
+        node *START;
+    public:
+        DoubleLinkedList(){
+            START = NULL;
+        }
+
+        void addnode(){
+            int nim;
+            cout << "\nEnter roll number of the student : ";
+            cin >> nim;
+
+            //step 1 : Allocate memory for new node
+            node *newnode = new node;
+        }
+
 };
